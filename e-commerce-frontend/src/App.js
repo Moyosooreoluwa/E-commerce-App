a883553408f9ffbe67491cba0c465714b9419bee
Moyosoore Ayo-Idowu <moyosooreai@gmail.com>
2022-09-21 11:39:15 +0100

    Part 3- Create-React-App

---
+++ e-commerce-frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Moyozon</a>
+      </header>
+      <main>Products</main>
+    </div>
+  );
+}
+
+export default App;
