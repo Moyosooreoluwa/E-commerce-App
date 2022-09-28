8015beab8e2a529cd60f316915008debcb41e669
Moyosoore Ayo-Idowu <moyosooreai@gmail.com>
2022-09-28 23:42:40 +0100

    Heroku (working)

--- e-commerce-backend/server.js
+++ e-commerce-backend/server.js
@@ -35,11 +35,11 @@ app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
 
 const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.use(express.static(path.join(__dirname, '/e-commerce-frontend/build')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/e-commerce-frontend/build/index.html'));
-});
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/e-commerce-frontend/build/index.html'))
+);
 
 // error handler
 app.use((err, req, res, next) => {
