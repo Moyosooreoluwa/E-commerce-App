a2ecb0f0dfd165c012491a93e8904da535830a0c
Moyosoore Ayo-Idowu <moyosooreai@gmail.com>
2022-09-28 22:39:18 +0100

    Heroku 2

--- e-commerce-backend/server.js
+++ e-commerce-backend/server.js
@@ -38,7 +38,7 @@ const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, '/frontend/build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'));
+  res.sendFile(path.join(__dirname, '/e-commerce-frontend/build/index.html'));
 });
 
 // error handler
